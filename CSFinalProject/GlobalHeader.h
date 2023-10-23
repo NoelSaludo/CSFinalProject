bfff826b8164f8554caa2fdfeccf448a430fdcb7
noel zyrence saludo <noelsaludo17@gmail.com>
2023-10-23 10:16:29 +0800

    Add project files.

---
+++ CSFinalProject/GlobalHeader.h
@@ -0,0 +1,11 @@
+///<summary>
+/// Ito po ang ating global header 
+/// pwede po natin gamitin ito anywhere sa ating files
+/// para hindi po natin needed na itype ito lagi sa ibang files
+/// 
+/// also paiwas po sa paggamit ng using namespace std dito
+///</summary>
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <vector>
---
+++ CSFinalProject/Source.cpp
@@ -0,0 +1,21 @@
+///<summary>
+/// Ito po yung repository ng ating Final Project para sa CS
+/// Notes para sa paggawa ng ating project:
+///		-Make sure na gumamit ng comments para laging informed kung para saan yung code pero wag naman po ispam
+///		-Make it readable as much as possible
+///		-Take it easy, kapag may di maayos na bug ay magpahinga ka po muna tsaka balikan
+/// 
+/// Ok yun lamang po at happy coding
+///</summary>
+
+//linker to global header
+#include "GlobalHeader.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+	cin.get();
+	return 0;
+}
