11179ed869afe4f43301ef0068048bd44a90316b
noel zyrence saludo <noelsaludo17@gmail.com>
2023-10-29 20:39:17 +0800

    ElectricEmission

--- CSFinalProject/Main.cpp
+++ CSFinalProject/Main.cpp
@@ -3,25 +3,39 @@
 
 using namespace std;
 
-void ElectricEmission()
+double ElectricEmission(double electricBill)
 {
-	//Enter calculation for ElectricEmission
+	return (electricBill / 56.98) * 105;
 }
 //add more calculations
 //remember that we are going to also check which emission causes the high emission 
 //so we can create a custom suggestion based on the calculation
 
+void TotalCarbonFootprint()
+{
+	//calculates total carbon footprint
+}
+
 int main()
 {
-	double electricBill,
-		gasBill,
-		oilBill,
-		carMileage;
+	//
+	double input,
+		electricEmission,
+		gasEmission,
+		oilEmission,
+		carEmission;
 	int flightsOverFourHours,
 		flightsUnderFourHours;
 	bool doesUserRecycleNewspaper,
 		doesUserRecycleAluminum;
 	//request for the user input
+	cout << "CarbonFootprin Calculator\n";
+	cin >> input;
+	cout << "Enter Electric Bill\n";
+	electricEmission = ElectricEmission(input);
+	//logs electricEmission
+	cout << " Output " << electricEmission;
+
 	cin.get();
 	return 0;
 }
